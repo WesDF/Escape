bda4beee41ba9579172e2fd57228a994e71cf0f4
Zer0Ph34r <sepheroth0504@yahoo.com>
2016-09-05 12:37:08 -0600

    Game STarted

---
+++ Escape/Assets/Scripts/ButtonBehaviorScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ButtonBehaviorScript : MonoBehaviour {
+
+	public void StartGame()
+    {
+        SceneManager.LoadScene("Story_0");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void instructionsPage()
+    {
+        SceneManager.LoadScene("InstructionsScene");
+    }
+}
